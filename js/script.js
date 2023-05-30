77500b42b43cd26dbec1737215a2a896f09d8e60
andreathepa <andreadepa32@gmail.com>
2023-05-30 17:30:23 +0200

    restart

--- js/script.js
+++ js/script.js
@@ -1,86 +1,202 @@
-// Stampare 5 numeri in pagina
-// Creare array
-// Stampare con innerhtml elementi in pagina
+let random_num = [];
 
 
-let numeri = ['3','5','6','11','20'];
-stampa_numeri = document.getElementById('numeri');
 
-for (i=0; i<numeri.length; i++){
 
-    let item = numeri[i];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Stampare 5 numeri in pagina
+// // Creare array
+// // Stampare con innerhtml elementi in pagina
+
+
+// let numeri = ['3','5','6','11','20'];
+// stampa_numeri = document.getElementById('numeri');
+
+// for (i=0; i<numeri.length; i++){
+
+//     let item = numeri[i];
     
-    stampa_numeri.innerHTML += `<li>${item}</li>`;
-}
+//     stampa_numeri.innerHTML += `<li>${item}</li>`;
+// }
     
-//Far sparire i numeri dopo 30 secondi (5secondi test)
+// //Far sparire i numeri dopo 30 secondi (5secondi test)
 
-let seconds = 5;
+// let seconds = 5;
 
-let interval = setInterval(function(){
-
-
-    if(seconds === 0){
-        clearInterval(interval);
-        stampa_numeri.innerHTML = ``;
+// let interval = setInterval(function(){
 
 
-    }
-    else{
-        seconds--;
+//     if(seconds === 0){
+//         clearInterval(interval);
+//         stampa_numeri.innerHTML = ``;
 
-    }
 
-}, 1000);
+//     }
+//     else{
+//         seconds--;
 
-//Messaggio di istruzioni
+//     }
 
-let messaggio = document.getElementById('messaggio')
-let chiedo_1;
-let chiedo_2;
+// }, 1000);
 
-setTimeout(function(){
+// //Messaggio di istruzioni
 
-    messaggio.innerHTML += `Ora scrivi uno alla volta i numeri che ricordi`;
+// let messaggio = document.getElementById('messaggio')
+// let chiedo_1;
+// let chiedo_2;
+// let chiedo_3;
+// let chiedo_4;
+// let chiedo_5;
 
-}, 6000)
+// let right = [];
+// let wrong = [];
 
-//Far apparire i prompt
+// setTimeout(function(){
 
-setTimeout(function(){
+//     messaggio.innerHTML += `Ora scrivi uno alla volta i numeri che ricordi`;
 
-    chiedo_1 = parseInt(prompt('Dimmi il primo numero che hai visto'));
+// }, 6000)
 
-    if(chiedo_1 == 3){
-        alert('Hai vinto')
-    }
-    else{
-        alert('hai perso')
-    }
-    
+// //Far apparire i prompt
 
-}, 11000)
+// setTimeout(function(){
 
-setTimeout(function(){
+//     chiedo_1 = parseInt(prompt('Dimmi il primo numero che hai visto'));
 
-    chiedo_2 = parseInt(prompt('Dimmi il secondo numero che hai visto'));
-
-    if(chiedo_2 == 5){
-        alert('Hai vinto')
-    }
-    else{
-        alert('hai perso')
-    }
+//     if(chiedo_1 == 3){
+//         right.push(chiedo_1)
+        
+//     }
+//     else{
+//         wrong.push(chiedo_1)
+//     }
     
 
-}, 11000)
+// }, 11000)
+
+// setTimeout(function(){
+
+//     chiedo_2 = parseInt(prompt('Dimmi il secondo numero che hai visto'));
+
+//     if(chiedo_2 == 5){
+//         right.push(chiedo_2)
+//     }
+//     else{
+//         wrong.push(chiedo_2)
+//     }
+    
+
+// }, 11000)
+
+// setTimeout(function(){
+
+//     chiedo_3 = parseInt(prompt('Dimmi il terzo numero che hai visto'));
+
+//     if(chiedo_3 == 6){
+//         right.push(chiedo_3)
+//     }
+//     else{
+//         wrong.push(chiedo_3)
+//     }
+    
+
+// }, 11000)
+
+// setTimeout(function(){
+
+//     chiedo_4 = parseInt(prompt('Dimmi il quarto numero che hai visto'));
+
+//     if(chiedo_4 == 11){
+//         right.push(chiedo_4)
+//     }
+//     else{
+//         wrong.push(chiedo_4)
+//     }
+    
+
+// }, 11000)
+
+// setTimeout(function(){
+
+//     chiedo_5 = parseInt(prompt('Dimmi il quinto numero che hai visto'));
+
+//     if(chiedo_5 == 20){
+//         right.push(chiedo_5)
+//     }
+//     else{
+//         wrong.push(chiedo_5)
+//     }
+
+       
+
+// }, 11000)
+
+// setTimeout(function(){
+
+//     let risultato_fin = document.getElementById('risultato_fin')
 
 
+//     if(right == numeri){
+
+//         risultato_fin.innerHTML += `Hai scritto correttamente tutti i numeri`;
+
+        
+//     }
+//     else{
+//         risultato_fin.innerHTML += `Hai perso`;
+
+//     }
+    
+
+// }, 11000)
 
 
+// console.log(right)
+// console.log(wrong)
+// let risultato_fin = document.getElementById('risultato_fin')
 
-// Far apparire 5 prompt
 
-// pushare i numeri in un'array
-// se tutti i numeri sono uguali all'array iniziale, allora il gioco finisce
-// altrimenti si riporta che ha perso
+// // Far apparire 5 prompt
+
+// // pushare i numeri in un'array
+// // se tutti i numeri sono uguali all'array iniziale, allora il gioco finisce
+// // altrimenti si riporta che ha perso
+
